be5c53976e01781232a6a1c0ffa17252935dfde2
hiro8_1999 <yanagida100101@gmail.com>
2019-11-10 21:18:21 +0900

    UI,ズーム追加

--- XCurator/Assets/Common.cs
+++ XCurator/Assets/Common.cs
@@ -13,6 +13,9 @@ namespace Common
 	public static class Define
 	{
 		public const int NUM_MAT_MAX = 10;
-	
+		public const float ZOOM_MIN = -20.0f;
+		public const float ZOOM_MAX = -5.0f;
+		public const float ZOOM_SPEED = 4.0f;
+
 	}
 }
---
+++ XCurator/Assets/key_input.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Common;
+
+public class key_input : MonoBehaviour
+{
+	public Camera camera;
+	private origin_mgr mgr;
+	
+	// Start is called before the first frame update
+	void Start()
+    {
+		mgr = gameObject.GetComponent<origin_mgr>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//回転
+		if (Input.GetKey(KeyCode.W))
+		{
+
+			transform.Rotate(new Vector3(5, 0, 0), Space.World);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Rotate(new Vector3(-5, 0, 0), Space.World);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(new Vector3(0, -5, 0), Space.World);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(new Vector3(0, 5, 0), Space.World);
+		}
+
+		//拡縮
+		float scroll = Input.GetAxis("Mouse ScrollWheel");
+		Vector3 pos_inst = camera.transform.position + new Vector3(0, 0, scroll * Define.ZOOM_SPEED);
+		if (scroll > 0 && pos_inst.z > Define.ZOOM_MAX)
+		{
+			pos_inst.z = Define.ZOOM_MAX;
+		}
+		else if (scroll < 0 && pos_inst.z < Define.ZOOM_MIN)
+		{
+			pos_inst.z = Define.ZOOM_MIN;
+		}
+		camera.transform.position = pos_inst;
+
+		mgr.ray = camera.ScreenPointToRay(Input.mousePosition);
+	}
+}
