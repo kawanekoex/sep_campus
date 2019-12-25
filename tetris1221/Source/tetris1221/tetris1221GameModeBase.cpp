17d9b7932ee616ac6952ca233652e3d9917cf12b
hiro8_1999 <yanagida100101@gmail.com>
2019-12-25 21:21:07 +0900

    no message

--- tetris1221/Source/tetris1221/tetris1221GameModeBase.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "tetris1221GameModeBase.h"
-#include"Private/Input.h"
-Atetris1221GameModeBase::Atetris1221GameModeBase() {
-	DefaultPawnClass = AInput::StaticClass();
-}
